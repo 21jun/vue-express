8ccd5041596bf3ff75269e9fc06932f6a667dcd3
21jun <21jun7654@gmail.com>
2018-08-08 11:08:41 +0900

    init

---
+++ frontend/src/components/ShowPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>상세 내용</h1>
+    <h2>{{movie[0].name}}</h2>
+    <img v-bind:src="movie[0].poster" class="poster">
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    created: function () {
+      var id = this.$route.params.id
+      console.log(id);
+      this.$http.get(`/api/movies/${id}`)
+        .then((response) => {
+          this.movie = response.data
+        })
+    },
+    data: function () {
+      return {
+        movie: {}
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
